5ea2cc3a0a4d3049acbb3534fd7c0c39295032ae
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2023-10-12 22:05:38 +0300

    commit two

--- tel.js
+++ tel.js
@@ -3,26 +3,23 @@
 // .then(response => response.json())
 //       .then(json => console.log(json))
 
-let url = "https://jsonplaceholder.typicode.com/photos"
-let t = document.querySelector(".h")
-const Api = async()=>{
-      let Api = await fetch(url)
-      let data = await Api.json()
-      let html = "";
-      
-      data.map((Element) => {
-            html+= ` <div class="t">
-            <img src="${Element.albumId}" alt="">
-            <p id="h">lorem</p>
+let url = "https://jsonplaceholder.typicode.com/photos";
+let t = document.querySelector(".t");
+
+const Api = async () => {
+  let Api = await fetch(url);
+  let data = await Api.json();
+  let html = "";
+
+  data.map((Element) => {
+    html += `<div class="t">
+            <img src="${Element.url}" alt="">
+            <p id="h">${Element.title}</p>
         </div>
-        </div>`
+        </div>`;
+  });
 
-      })
-      
-      
-      t.innerHTML = html;
+  t.innerHTML = html;
+};
 
-}
-
-
-Api()
+Api();
